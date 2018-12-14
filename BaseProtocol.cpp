179b8b948822bdac9387417752ce140520926fb1
zhouyushuang <zhouyushuang@onescorpion.com>
2018-12-14 11:46:00 +0800

    新增
    添加protocol基类

---
+++ BaseProtocol.cpp
@@ -0,0 +1,78 @@
+#include "BaseProtocol.h"
+#include "Transport.h"
+
+#include <boost/make_shared.hpp>
+#include <boost/chrono.hpp>
+#include <boost/chrono/duration.hpp>
+#include <boost/bind.hpp>
+
+#include <arpa/inet.h>
+
+#include <iostream>
+
+CBaseProtocol::CBaseProtocol(boost::asio::io_context &ioc, boost::shared_ptr<CBaseTransport> transport) :
+    m_ioc(ioc),
+    m_transport(transport)
+{
+    if (m_transport)
+    {
+        m_transport->set_on_connected(boost::bind(&CBaseProtocol::on_connected, this));
+        m_transport->set_on_disconnected(boost::bind(&CBaseProtocol::on_disconnected, this));
+        m_transport->set_on_data_received(boost::bind(&CBaseProtocol::on_data_received, this, _1));
+    }
+}
+
+CBaseProtocol::~CBaseProtocol()
+{
+
+}
+
+void CBaseProtocol::write(const std::string &message)
+{
+    if (m_transport)
+    {
+        m_transport->write(message, boost::bind(&CBaseProtocol::on_write_error, this, _1));
+    }
+}
+
+void CBaseProtocol::close()
+{
+    if (m_transport)
+    {
+        m_transport->disconnect();
+    }
+}
+
+int CBaseProtocol::transport_status()
+{
+    if (m_transport)
+    {
+        return m_transport->status();
+    }
+    return CBaseTransport::EN_CLOSE;
+}
+
+void CBaseProtocol::on_connected()
+{
+
+}
+
+void CBaseProtocol::on_disconnected()
+{
+
+}
+
+void CBaseProtocol::on_write_error(const std::string &data)
+{
+
+}
+
+void CBaseProtocol::message_received(const std::string &message)
+{
+
+}
+
+void CBaseProtocol::on_data_received(const std::string &data)
+{
+
+}
---
+++ BaseProtocol.h
@@ -0,0 +1,75 @@
+#ifndef BASEPROTOCOL_H
+#define BASEPROTOCOL_H
+
+#include <boost/asio.hpp>
+
+
+class Transport;
+typedef Transport CBaseTransport;
+
+/**
+ * @brief The CBaseProtocol class 组装业务数据基类，并将数据发送到对端
+ */
+class CBaseProtocol
+{
+public:
+    CBaseProtocol(boost::asio::io_context &ioc, boost::shared_ptr<CBaseTransport> transport);
+
+    ~CBaseProtocol();
+
+public:
+    /**
+     * @brief write 发送数据到对对端
+     * @param message 消息
+     */
+    virtual void write(const std::string &message);
+
+    /**
+     * @brief close 关闭连接
+     */
+    void close();
+
+    /**
+     * @brief transport_status 获取连接状态
+     * @return  EN_READY
+                EN_OK
+                EN_CLOSE
+     */
+    int transport_status();
+
+protected:
+    /**
+     * @brief on_connected 连接创建成功后业务层回调函数
+     */
+    virtual void on_connected();
+
+    /**
+     * @brief on_disconnected 连接重置后业务层回调函数
+     */
+    virtual void on_disconnected();
+
+    /**
+     * @brief on_write_error
+     * @param data
+     */
+    virtual void on_write_error(const std::string &data);
+
+protected:
+    /**
+     * @brief data_received 数据接收回调
+     * @param message 消息
+     */
+    virtual void message_received(const std::string &message);
+
+    /**
+     * @brief on_data_received 对收到的消息进行处理
+     * @param data
+     */
+    virtual void on_data_received(const std::string &data);
+
+protected:
+    boost::asio::io_context &m_ioc;
+    boost::shared_ptr<CBaseTransport> m_transport;
+};
+
+#endif // BASEPROTOCOL_H
